9ba839d45cab7de38a5c67ede34bdb12982341c1
uo277891 <79696349+uo277891@users.noreply.github.com>
2021-11-29 11:46:45 +0100

    Add files via upload

--- JavaScript/ejercicio01/Parrafos.js
+++ JavaScript/ejercicio01/Parrafos.js
@@ -17,7 +17,7 @@ document.write("<p>Curso actual: ");
 document.write(asignatura.curso);
 document.write("</p>");
 document.write("<p>Alumno: ");
-document.write(asignatura.profesor);
+document.write(asignatura.alumno);
 document.write("</p>");
 document.write("<p>e-mail: ");
 document.write(asignatura.email);
